12bb23051d683d778a39e899559a95b91b4d4c97
FluxMeister <doctorpauk@gmail.com>
2021-06-30 11:05:36 +0200

    Nicer background colour

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
-import './ExpenseItem.css'
+import './ExpenseItem.css';
 
 function ExpenseItem() {
   return (
     <div className="expense-item">
-      <div>June 30th 2021</div>
-      <div>
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>€294.67</div>
+        <div className="expense-item__price">$294.67</div>
       </div>
     </div>
   );
