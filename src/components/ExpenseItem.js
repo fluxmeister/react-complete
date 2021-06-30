9170b9a4cad27a2a3fea128d078355ec8e88382a
FluxMeister <doctorpauk@gmail.com>
2021-06-30 03:50:22 +0200

    First Component code.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expenseitem!</h2>
+  return (
+    <div>
+      <div>June 30th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>€294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
