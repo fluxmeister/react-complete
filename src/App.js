8e3316902281cf6d57d21a473ecf0ede2307fb8c
FluxMeister <doctorpauk@gmail.com>
2021-07-02 09:51:52 +0200

    UI translated to cyrilic

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ const App = () => {
   const expenses = [
     {
       id: 'e1',
-      title: 'Toalet papir',
+      title: 'Тоалет папир',
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'Novi TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'Нови ТВ', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
-      title: 'Osiguranje vozila',
+      title: 'Осигурање возила',
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
       id: 'e4',
-      title: 'Novi sto (Drveni)',
+      title: 'Нови сто (дрвени)',
       amount: 450,
       date: new Date(2021, 5, 12),
     },
