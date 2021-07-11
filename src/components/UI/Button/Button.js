e6471a86d18c92bcdf81241e22139304f7c4db64
FluxMeister <doctorpauk@gmail.com>
2021-07-11 05:24:39 +0200

    06-007 Using CSS Modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,46 +1,56 @@
-import styled from 'styled-components';
+import React from "react";
 
-const Button = styled.button`
-    width: 100%;
-    font: inherit;
-    padding: 0.5rem 1.5rem;
-    border: 1px solid #8b005d;
-    color: white;
-    font-weight: bold;
-    background: #8b005d;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-    cursor: pointer;
-    border-radius: 5px;
-    background: linear-gradient(
-      144deg,
-      rgba(2, 0, 36, 1) 0%,
-      rgba(121, 9, 100, 1) 33%,
-      rgba(0, 212, 255, 1) 100%
-    );
+import styles from './Button.module.css';
 
-    @media (min-width: 768px) {
-      width: auto;
-    }
+// import styled from 'styled-components';
 
-  &:focus {
-    outline: none;
-  }
+// const Button = styled.button`
+//     width: 100%;
+//     font: inherit;
+//     padding: 0.5rem 1.5rem;
+//     border: 1px solid #8b005d;
+//     color: white;
+//     font-weight: bold;
+//     background: #8b005d;
+//     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//     cursor: pointer;
+//     border-radius: 5px;
+//     background: linear-gradient(
+//       144deg,
+//       rgba(2, 0, 36, 1) 0%,
+//       rgba(121, 9, 100, 1) 33%,
+//       rgba(0, 212, 255, 1) 100%
+//     );
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
+//     @media (min-width: 768px) {
+//       width: auto;
+//     }
 
-`; // button is a method in an object 'styled'
+//   &:focus {
+//     outline: none;
+//   }
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+
+// `; // button is a method in an object 'styled'
+
+
+
+
+
+
+
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
