ccc7b619e144ce00925ffb096c4826659e05f35f
FluxMeister <doctorpauk@gmail.com>
2021-07-11 03:27:26 +0200

    Styled colors personally

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,15 +1,21 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-
-  
     font: inherit;
     padding: 0.5rem 1.5rem;
     border: 1px solid #8b005d;
     color: white;
+    font-weight: bold;
     background: #8b005d;
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
     cursor: pointer;
+    border-radius: 5px;
+    background: linear-gradient(
+      144deg,
+      rgba(2, 0, 36, 1) 0%,
+      rgba(121, 9, 100, 1) 33%,
+      rgba(0, 212, 255, 1) 100%
+    );
 
   &:focus {
     outline: none;
