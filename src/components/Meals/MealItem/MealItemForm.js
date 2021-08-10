3addcddbcac2721194eadc01a676eed1d618bfa6
FluxMeister <doctorpauk@gmail.com>
2021-08-10 18:57:32 +0200

    11-007 Adding a Form

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -6,6 +6,14 @@ const MealItemForm = (props) => {
     <form className={classes.form}>
       <Input
         label="Amount"
+        /**
+         * input prop holds another object by itself.
+         * So, the value is an object, hence the double curly braces.
+         * One pair of curly braces to evaluate a JavaScript expression 
+         * which is passed in as a value. And then expression just 
+         * happens to be a JavaScript object.
+         * Hence, we have that other pair of curly braces.
+         */
         input={{
           id: "amount",
           type: "number",
@@ -14,6 +22,10 @@ const MealItemForm = (props) => {
           step: "1",
           defaultValue: "1",
         }}
+        /**
+         * We just distribute these validation props onto the input
+         * <input {...props.input} />
+         */
       />
       <button>+ Add</button>
     </form>
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -5,6 +5,13 @@ const Input = props => {
         <label htmlFor={props.input.id}>{props.label}</label>
         <input {...props.input} />
     </div>
+    /* We use the spread operator to spread props.input.
+    This ensures that all the key-value pairs in this input object, 
+    which we receive on props.input, 
+    are added as props to input.
+    It's simply a convenient way of making this input highly configurable
+    from outside this component, through this input prop here.
+    */
 };
 
 export default Input;
